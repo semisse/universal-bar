e2ec89ba4372a5c749a694cbe74701552a0d6f3a
Samuel Fialho <sfialho@nuxeo.com>
2019-07-03 18:15:07 +0100

    ELEMENTS-990: add nuxeo-data-table stories

--- storybook/.storybook/theme.js
+++ storybook/.storybook/theme.js
@@ -4,5 +4,5 @@ export default create({
   base: 'light',
   brandTitle: 'Nuxeo Elements Catalog',
   brandUrl: 'https://nuxeo.com',
-  brandImage: 'https://www.nuxeo.com/assets/imgs/logo340x60.png',
+  brandImage: '/nuxeo-elements-catalog.svg',
 });
